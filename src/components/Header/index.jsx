5c436c5a7c29355fc0ca22d9a3f578c268bdd933
gregolly <gregollyff@hotmail.com>
2024-01-07 18:43:06 -0400

    feat: hiding label on input search when user is typing

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,48 @@
-import { Button } from '../Button'
-import { Logo } from '../Logo'
-import { SearchIcon } from '../SeachIcon'
-import { Container, SearchContainer } from "./styles"
+import { useCallback, useState } from 'react'
 
+import { Button } from '../Button'
+
+import { Logo } from '../Logo'
+import { SearchIcon } from '../SearchIcon'
 import { ReceiptIcon } from '../ReceiptIcon'
 import { SignOutIcon } from '../SignOutIcon'
 
+import { Container, SearchContainer } from "./styles"
+
 export const Header = () => {
+    const [hasValueOnSearchInput, setHasValueOnSearchInput] = useState(null)
+
+    const onChangeInputSearch = useCallback((event) => {
+        const inputValue = event.target.value
+        setHasValueOnSearchInput(inputValue.length > 0 ? inputValue : null)
+    }, [])
+
+    const isUserAdmin = false
+
     return (
         <Container>
             <Logo />
             <SearchContainer>
-                <div>
+                {!hasValueOnSearchInput ? (
+                    <div>
                     <SearchIcon width={25} height={24} />
-                    <label>Busque por pratos ou ingredientes</label>
+                    <label htmlFor='search'>Busque por pratos ou ingredientes</label>
                 </div>
-                <input type="text" />
+                ) : (
+                    ""
+                )}
+                <input type="text" id='search' onChange={onChangeInputSearch} />
             </SearchContainer>
-            <Button>
+            {isUserAdmin ? (
+                <Button>
+                    Novo prato
+                </Button>
+            ) : (
+                <Button>
                     <ReceiptIcon width={33} height={32} />
                     Pedidos (0)
-            </Button>
+                </Button>
+            )}
             <SignOutIcon width={32} height={32} />
         </Container>
     )
