84de3a4efb738f8e867852440e0f9bdef4a08b8f
gregolly <gregollyff@hotmail.com>
2024-01-07 01:32:11 -0400

    feat: add logo component

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,11 @@
+import { Polygon } from '../../components/Polygon'
+import { Container } from './styles'
+
+export const Logo = () => {
+    return (
+        <Container>
+                <Polygon />
+                <h1>Food Explorer</h1>
+        </Container>
+    )
+}
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+
+    h1 {
+        font-size: 42px;
+        font-weight: 700;
+        color: #fff;
+    }
+`
