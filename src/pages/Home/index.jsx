e8df460bdffda71543e14788be318a6e949eec9f
gregolly <gregollyff@hotmail.com>
2024-01-07 21:16:42 -0400

    feat: change name and removing header

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,9 @@
-import { Banners } from "../../components/Banners"
-import { Header } from "../../components/Header"
+import { Banner } from "../../components/Banner"
 
 export const Home = () => {
     return (
         <>
-            <Header />
-            <Banners />
+            <Banner />
         </>
     )
 }
