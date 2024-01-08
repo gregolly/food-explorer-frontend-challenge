9459900cc21b43a25360cd92bba5f0b258b3a7cd
gregolly <gregollyff@hotmail.com>
2024-01-08 12:54:44 -0400

    feat: add content to Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,15 @@
+
+import { Polygon } from "./components/Polygon"
 import { Container } from "./styles"
 
 export const Footer = () => {
     return (
         <Container>
-
+            <div>
+                <Polygon />
+                <h3>food explorer</h3>
+            </div>
+            <p>© 2023 - Todos os direitos reservados.</p>
         </Container>
     )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components'
 
-export const Container = styled.footer``
+export const Container = styled.footer`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: ${({ theme }) => theme.DARK[600]};
+    padding: 24px 120px;
+
+    > div {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        
+        h3 {
+            font-size: 24px;
+            font-weight: 700;
+            color: ${({ theme }) => theme.LIGHT[700]};
+        }
+    }
+`
