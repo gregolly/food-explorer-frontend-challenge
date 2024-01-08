ee9f16dde2e2a723818b2a61a3f331dbf304a334
gregolly <gregollyff@hotmail.com>
2024-01-07 20:41:45 -0400

    fix: remove style

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,22 +3,4 @@ import styled from "styled-components"
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-
-    label {
-        font-family: inherit;
-        font-size: 16px;
-        font-weight: 400;
-        line-height: 100%;
-        margin-bottom: 8px;
-        color: #C4C4CC;
-    }
-
-    input {
-        padding: 18px 14px;
-        border-radius: 8px;
-        background-color: #0D1D25;
-        border: none;
-        color: #fff;
-        width: 380px;
-    }
 `
